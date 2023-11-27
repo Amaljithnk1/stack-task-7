d56d216fbbaa343466c382eb8fd276b485e5d7e5
Amaljithnk1 <amajithnk@gmail.com>
2023-11-27 13:43:29 +0530

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "https://stack-overflow-xyev.onrender.com/"})
+const API = axios.create({baseURL: "https://stack-overflow-xyev.onrender.com"})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
